2426d7a724f22948c54cd81f86070e668e001fab
Anmay Gupta <anmaygupta1@gmail.com>
2022-02-28 22:44:48 -0600

    community start

--- src/pages/community/CommunityEditor.js
+++ src/pages/community/CommunityEditor.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/general/Navbar";
+import { useNavigate, useSearchParams } from "react-router-dom"
+import { onValue } from "firebase/database";
+import { rdb, db, auth } from "../../firebase";
 
 function CommunityEditor() {
-  return <div>Community Editor</div>;
+  const [params] = useSearchParams();
+  const docId = params.get("id");
+  const dbRef = ref(rdb, `/${docId}`);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Navigate to 404 if doc does not exist
+    var dbRefConnected = false;
+    onValue(dbRef, (snapshot) => {
+      if (snapshot.val() !== null && !dbRefConnected) {
+        const data = snapshot.val().value;
+      } else if (snapshot.val() === null) {
+        navigate("/404")
+      }
+    })
+  
+    return () => {
+      // Clear up the async to prevent memory leaks
+    }
+  }, [])
+  
+
+  return (
+    <div className="bg-white">
+      <Navbar />
+
+      <div>Here</div>
+    </div>
+  );
 }
 
 export default CommunityEditor;
--- src/pages/community/CommunityHome.js
+++ src/pages/community/CommunityHome.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Navbar from "../../components/general/Navbar";
 import Footer from "../../components/general/Footer";
+import { useNavigate } from "react-router-dom";
 
 const CommunityHome = () => {
+  const navigate = useNavigate()
+
   return (
     <div className="h-screen bg-gray-100">
       <Navbar />
       <div className="h-full">
         <div className="flex items-center justify-center">
-          <button className="m-4 rounded-full bg-green-500 p-4 text-white font-extrabold text-xl">
+          <button
+            className="m-4 rounded-full bg-green-500 p-4 text-white font-extrabold text-xl"
+            onClick={() => {
+              navigate("/mdeditor")
+            }}
+          >
             Get started
           </button>
         </div>
